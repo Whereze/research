dbef8e165e9b0e773cd994406962c89a2b046f9b
Anna <anna.dvoskina@yandex.ru>
2021-10-01 09:40:30 +0300

    added parsing of the second page in wiki

--- parse_wiki_waterfalls.py
+++ parse_wiki_waterfalls.py
@@ -2,6 +2,8 @@ from bs4 import BeautifulSoup
 from bs4.builder import HTMLTreeBuilder
 import requests
 import wikipedia
+import csv
+import re
 wikipedia.set_lang("ru")
 
 
@@ -14,31 +16,50 @@ def get_html(url):
         print("Сетевая ошибка")
         return False
 
-
 def get_waterfalls():
     html = get_html("https://ru.wikipedia.org/wiki/Категория:Водопады_по_алфавиту")
     if html:
         soup = BeautifulSoup(html,features="html.parser")
         all_waterfalls = soup.findAll("li")
-        
-        result = []
-            
+        result = []    
         for waterfall in all_waterfalls:
             title =  waterfall.find('a')
             # url = waterfall.find('a')
             if title:
-                
                     title = title.text
                     # url = url['href']
                     result.append({
                         "title":title,
                         # "url":url,
                     })
-        return  result     
+        return result
     return False     
 
-            
-waterfalls_result = get_waterfalls()
+def get_waterfalls_second_page():
+    html = get_html("https://ru.wikipedia.org/w/index.php?title=%D0%9A%D0%B0%D1%82%D0%B5%D0%B3%D0%BE%D1%80%D0%B8%D1%8F:%D0%92%D0%BE%D0%B4%D0%BE%D0%BF%D0%B0%D0%B4%D1%8B_%D0%BF%D0%BE_%D0%B0%D0%BB%D1%84%D0%B0%D0%B2%D0%B8%D1%82%D1%83&pagefrom=%D0%A0%D1%8C%D1%8E%D0%BA%D0%B0%D0%BD%D1%84%D0%BE%D1%81%D1%81%D0%B5%D0%BD%0A%D0%A0%D1%8C%D1%8E%D0%BA%D0%B0%D0%BD%D1%84%D0%BE%D1%81%D1%81%D0%B5%D0%BD#mw-pages")
+    if html:
+        soup = BeautifulSoup(html,features="html.parser")
+        all_waterfalls = soup.findAll("li")
+        result = []    
+        for waterfall in all_waterfalls:
+            title =  waterfall.find('a')
+            # url = waterfall.find('a')
+            if title:
+                    title = title.text
+                    # url = url['href']
+                    result.append({
+                        "title":title,
+                        # "url":url,
+                    })
+        return result
+    return False  
+
+
+result_first_page = get_waterfalls()
+result_second_page = get_waterfalls_second_page()
+waterfalls_result = result_first_page + result_second_page
+
+
 def get_waterfalls_pages():
     waterfalls_list = []
     for water in waterfalls_result:
@@ -56,8 +77,6 @@ def get_waterfalls_pages():
         except (wikipedia.exceptions.DisambiguationError,wikipedia.exceptions.PageError):
             continue
     return waterfalls_list
-                
 
-if __name__ == "__main__":
-    result = get_waterfalls_pages()
-    print(result)
+# if __name__ == "__main__":
+#     print(get_waterfalls_pages())
